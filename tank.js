f7534b6a57810ebf5277025d22d26216a5009d39
luberechavarria33 <39109391+luberechavarria33@users.noreply.github.com>
2018-05-09 16:27:23 +1000

    Add files via upload

--- tank.js
+++ tank.js
@@ -1,1 +1,39 @@
+function checkJquery(){
+  if(typeof $ === "function"){
+    console.log("jquery has load");
+     main()
+
+  }else{
+    setTimeout(checkJquery, 100);
+   };
+};
+checkJquery()
+
+  function main(){
+var forward ="http://creator.kitsunei.com/hook/1d8cc980/forward";
+var back ="http://creator.kitsunei.com/hook/1d8cc980/back";
+var left ="http://creator.kitsunei.com/hook/1d8cc980/left";
+var right ="http://creator.kitsunei.com/hook/1d8cc980/right";
+
+    $( "#forward").click(function(){
+          $.ajax({url:forward});
+     });
+
+     $( "#back").click(function(){
+       
+        $.ajax({url:back});
+    });
+
+    $( "#back").click(function(){
+       
+        $.ajax({url:left});
+    });
+
+    $( "#back").click(function(){
+       
+        $.ajax({url:right});
+    });
+     
+ };
+
 
